f47e8eea562f064e1e4536577b2eab10902c0e3d
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-05-01 16:16:41 +0300

    Input handling like a boss

--- src/js/searchToggle.js
+++ src/js/searchToggle.js
@@ -1,5 +1,6 @@
 const searchButton = document.querySelector(".js-search");
 const searchBox = document.querySelector(".site-header__search");
+const searchInput = searchBox.childNodes[1];
 const CLASS_NAME = "active";
 
 searchButton.addEventListener("click", toggleSearch);
@@ -8,15 +9,31 @@ window.addEventListener("click", outClick);
 
 function toggleSearch(event) {
   event.stopPropagation();
-  this.classList.toggle(CLASS_NAME);
+  if (searchButton.classList.contains(CLASS_NAME)) {
+    hideSearch();
+  } else {
+    showSearch();
+  }
 }
 
 function outClick(event) {
   if (searchButton.classList.contains(CLASS_NAME)) {
-    searchButton.classList.remove(CLASS_NAME);
+    hideSearch();
   }
 }
 
 function killSearchClick(event) {
   event.stopPropagation();
+}
+
+function showSearch() {
+  searchInput.focus();
+  searchButton.classList.add(CLASS_NAME);
+}
+
+function hideSearch() {
+  searchButton.classList.remove(CLASS_NAME);
+  searchInput.blur();
+  searchInput.value = "";
+  console.log(searchInput.value);
 }
